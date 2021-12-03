a336317ce78d250bcb60c9cd9a10e546ca39afb5
rajesh <raaj.everest@gmail.com>
2021-12-03 14:28:58 +0530

    added departure date in Home and search Page

--- src/main/java/com/everest/airline/Data.java
+++ src/main/java/com/everest/airline/Data.java
@@ -1,13 +1,17 @@
 package com.everest.airline;
 
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.Calendar;
+import java.util.GregorianCalendar;
 import java.util.List;
 
 public class Data {
     public static List<Flight> flights = List.of(
-            new Flight(1001, "Hyderabad", "Bangalore"),
-            new Flight(1002, "Bangalore", "Hyderabad"),
-            new Flight(1003, "Goa", "Bangalore"),
-            new Flight(1004, "Bangalore", "Goa"),
-            new Flight(1005, "Bangalore", "Hyderabad"),
-            new Flight(1006, "Hyderabad", "Bangalore"));
+            new Flight(1001, "Hyderabad", "Bangalore",LocalDate.of(2021, Month.DECEMBER, 6)),
+            new Flight(1002, "Bangalore", "Hyderabad",LocalDate.of(2021, Month.DECEMBER, 7)),
+            new Flight(1003, "Goa", "Bangalore",LocalDate.of(2021, Month.DECEMBER, 8)),
+            new Flight(1004, "Bangalore", "Goa",LocalDate.of(2021, Month.DECEMBER, 9)),
+            new Flight(1005, "Bangalore", "Hyderabad",LocalDate.of(2021, Month.DECEMBER, 10)),
+            new Flight(1006, "Hyderabad", "Bangalore",LocalDate.of(2021, Month.DECEMBER, 11)));
 }
--- src/main/java/com/everest/airline/Flight.java
+++ src/main/java/com/everest/airline/Flight.java
@@ -1,14 +1,18 @@
 package com.everest.airline;
 
+import java.time.LocalDate;
+
 public class Flight {
     private long number;
     private String source;
     private String destination;
+    private LocalDate departureDate;
 
-    public Flight(long number, String source, String destination) {
+    public Flight(long number, String source, String destination, LocalDate departureDate) {
         this.number = number;
         this.source = source;
         this.destination = destination;
+        this.departureDate = departureDate;
     }
 
     public long getNumber() {
@@ -23,4 +27,7 @@ public class Flight {
         return destination;
     }
 
+    public LocalDate getDepartureDate() {
+        return departureDate;
+    }
 }
