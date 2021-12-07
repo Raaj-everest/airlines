a51cb2c08a0ae66f63b011a2b8c6ba865754537e
rajesh <raaj.everest@gmail.com>
2021-12-07 11:41:41 +0530

    Story-4 completed

--- src/main/java/com/everest/airline/Data.java
+++ src/main/java/com/everest/airline/Data.java
@@ -1,17 +1,42 @@
 package com.everest.airline;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.time.LocalDate;
 import java.time.Month;
-import java.util.Calendar;
-import java.util.GregorianCalendar;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.Scanner;
 
 public class Data {
-    public static List<Flight> flights = List.of(
-            new Flight(1001, "Hyderabad", "Bangalore",LocalDate.of(2021, Month.DECEMBER, 6),9),
-            new Flight(1002, "Bangalore", "Hyderabad",LocalDate.of(2021, Month.DECEMBER, 7),8),
-            new Flight(1003, "Goa", "Bangalore",LocalDate.of(2021, Month.DECEMBER, 8),7),
-            new Flight(1004, "Bangalore", "Goa",LocalDate.of(2021, Month.DECEMBER, 9),6),
-            new Flight(1005, "Bangalore", "Hyderabad",LocalDate.of(2021, Month.DECEMBER, 10),8),
-            new Flight(1006, "Hyderabad", "Bangalore",LocalDate.of(2021, Month.DECEMBER, 6),5));
+//    public static List<Flight> flights = List.of(
+//            new Flight(1001, "Hyderabad", "Bangalore", LocalDate.of(2021, Month.DECEMBER, 6), 9),
+//            new Flight(1002, "Bangalore", "Hyderabad", LocalDate.of(2021, Month.DECEMBER, 7), 8),
+//            new Flight(1003, "Goa", "Bangalore", LocalDate.of(2021, Month.DECEMBER, 8), 7),
+//            new Flight(1004, "Bangalore", "Goa", LocalDate.of(2021, Month.DECEMBER, 9), 6),
+//            new Flight(1005, "Bangalore", "Hyderabad", LocalDate.of(2021, Month.DECEMBER, 10), 8),
+//            new Flight(1006, "Hyderabad", "Bangalore", LocalDate.of(2021, Month.DECEMBER, 6), 5));
+
+    public static List<Flight> readDataFromFile() throws FileNotFoundException {
+
+        Scanner in = new Scanner(new FileReader("/Users/raaj/projects/airlines/src/main/java/com/everest/airline/data.txt"));
+        List<Flight> flightsFromFile = new ArrayList<>();
+        String flightData;
+        while (in.hasNextLine()) {
+            flightData = in.next();
+            String[] flight = flightData.split(",");
+            int flightNumber = Integer.parseInt(flight[0]);
+            String source = flight[1];
+            String destination = flight[2];
+            String[] departureDate = flight[3].split("-");
+            int year = Integer.parseInt(departureDate[0]);
+            int month = Integer.parseInt(departureDate[1]);
+            int day = Integer.parseInt(departureDate[2]);
+            int occupiedSeats = Integer.parseInt(flight[4]);
+            flightsFromFile.add(new Flight(flightNumber, source, destination, LocalDate.of(year, month, day), occupiedSeats));
+        }
+        return flightsFromFile;
+    }
 }
--- src/main/java/com/everest/airline/SearchController.java
+++ src/main/java/com/everest/airline/SearchController.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.io.FileNotFoundException;
 import java.time.LocalDate;
 
 @Controller
@@ -16,7 +17,7 @@ public class SearchController {
     }
 
     @RequestMapping(value = "/search")
-    public String search(String from, String to, String ticket, String departureDate, Model model) {
+    public String search(String from, String to, String ticket, String departureDate, Model model) throws FileNotFoundException {
         model.addAttribute("flights", SearchHelper.sourceToDestination(from, to, LocalDate.parse(departureDate), Integer.valueOf(ticket)));
         return "search";
     }
