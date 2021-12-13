650d680e639074ec41aa9b841ac9120e9c6498b0
rajesh <raaj.everest@gmail.com>
2021-12-13 22:31:16 +0530

    removed unused functions in Data class

--- src/main/java/com/everest/airline/Data.java
+++ src/main/java/com/everest/airline/Data.java
@@ -1,6 +1,8 @@
 package com.everest.airline;
 
-import java.io.*;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
@@ -17,58 +19,6 @@ public class Data {
                 .filter(Files::isRegularFile)
                 .map(Path::toFile)
                 .collect(Collectors.toList());
-    }
-
-    public static List<Flight> readDataFromFile() throws FileNotFoundException {
-
-        Scanner in = new Scanner(new FileReader("/Users/raaj/projects/airlines/src/main/java/com/everest/airline/data.txt"));
-        List<Flight> flightsFromFile = new ArrayList<>();
-        String flightData;
-        while (in.hasNextLine()) {
-            flightData = in.nextLine();
-            String[] flight = flightData.split(",");
-            int flightNumber = Integer.parseInt(flight[0]);
-            String source = flight[1];
-            String destination = flight[2];
-            String[] departureDate = flight[3].split("-");
-            int year = Integer.parseInt(departureDate[0]);
-            int month = Integer.parseInt(departureDate[1]);
-            int day = Integer.parseInt(departureDate[2]);
-            int capacity = Integer.parseInt(flight[4]);
-            int occupiedSeats = Integer.parseInt(flight[5]);
-            flightsFromFile.add(new Flight(flightNumber, source, destination, LocalDate.of(year, month, day), capacity, occupiedSeats));
-        }
-        return flightsFromFile;
-    }
-
-    public static void writeToFile(List<Flight> ff) {
-        try {
-            FileWriter myWriter = new FileWriter("/Users/raaj/projects/airlines/src/main/java/com/everest/airline/data.txt");
-            for (Flight f :
-                    ff) {
-                myWriter.write(String.valueOf(f.getNumber()));
-                myWriter.write(",");
-                myWriter.write(f.getSource());
-                myWriter.write(",");
-                myWriter.write(f.getDestination());
-                myWriter.write(",");
-                myWriter.write(String.valueOf(f.getDepartureDate().getYear()));
-                myWriter.write("-");
-                myWriter.write(String.valueOf(f.getDepartureDate().getMonthValue()));
-                myWriter.write("-");
-                myWriter.write(String.valueOf(f.getDepartureDate().getDayOfMonth()));
-                myWriter.write(",");
-                myWriter.write(String.valueOf(f.getCapacity()));
-                myWriter.write(",");
-                myWriter.write(String.valueOf(f.getTotalOccupiedSeats()));
-                myWriter.write("\n");
-            }
-            myWriter.close();
-        } catch (IOException e) {
-            System.out.println("An error occurred.");
-            e.printStackTrace();
-        }
-
     }
 
     public static List<Flight> readFromFiles() throws IOException {
@@ -90,7 +40,6 @@ public class Data {
             int occupiedSeats = Integer.parseInt(flight[5]);
             flightsFromFile.add(new Flight(flightNumber, source, destination, LocalDate.of(year, month, day), capacity, occupiedSeats));
         }
-
         return flightsFromFile;
     }
 
