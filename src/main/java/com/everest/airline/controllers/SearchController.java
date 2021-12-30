875003e01fd7be1635555d0a12b625f963a890d9
rajesh <raaj.everest@gmail.com>
2021-12-30 13:36:14 +0530

    refactored search controller

--- src/main/java/com/everest/airline/controllers/SearchController.java
+++ src/main/java/com/everest/airline/controllers/SearchController.java
@@ -10,8 +10,10 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.io.File;
 import java.io.IOException;
 import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -33,11 +35,18 @@ public class SearchController {
 
 
     @RequestMapping(value = "/search")
-    public String search(String from, String to, String departureDate, String numberOfPassengersBoarding, String classType, Model model) {
+    public String search(String from, String to, String departureDate, String numberOfPassengersBoarding, String classType, Model model) throws IOException {
         if (from != null) {
             setValues(from, to, departureDate, numberOfPassengersBoarding, classType);
         }
-        List<Flight> searchedFlights = dataReader.getAllFlightsFromFiles().stream()
+        List<Flight> data = new ArrayList<>();
+            File[] files = dataReader.getListOfFiles();
+            for (File file : files) {
+                String flightData = dataReader.readFile(file);
+                Flight flight = dataReader.stringToFlight(flightData);
+                data.add(flight);
+            }
+            List<Flight> searchedFlights = data.stream()
                 .filter(flight -> (flight.getSource().equalsIgnoreCase(this.from) && flight.getDestination().equalsIgnoreCase(this.to) && flight.getDepartureDate().equals(this.departureDate) && (flight.checkAvailability(this.classType, this.numberOfPassengersBoarding))))
                 .collect(Collectors.toList());
         if (searchedFlights.size() == 0) {
@@ -50,9 +59,11 @@ public class SearchController {
 
     @RequestMapping(value = "/{number}")
     public String book(@PathVariable("number") String number, Model model) throws IOException {
-        List<Flight> flights = dataReader.getAllFlightsFromFiles().stream().filter(f -> f.getNumber() == Long.parseLong(number)).collect(Collectors.toList());
-        flights.get(0).updateOccupiedSeats(classType, numberOfPassengersBoarding);
-        dataWriter.writingToFiles(flights.get(0));
+        File file = dataReader.getFile(String.valueOf(number));
+        String flightData = dataReader.readFile(file);
+        Flight flight = dataReader.stringToFlight(flightData);
+        flight.updateOccupiedSeats(classType, numberOfPassengersBoarding);
+        dataWriter.writingToFiles(flight);
         return "confirmed";
     }
 
